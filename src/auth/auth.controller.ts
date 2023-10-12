a868587298ebff131847f61c3b40506c83e661e1
Clément <clab60917@eleve.isep.fr>
2023-10-12 13:11:25 +0200

    requete localhsot working signup/signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('signup')
   signup() {
     return 'I am sign up';
   }
+
   @Post('signin')
   login() {
     return 'I am sign in';
