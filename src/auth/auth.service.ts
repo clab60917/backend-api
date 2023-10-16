d61635dad9ab28255882e80ef6ba4cf56dc5e6b2
Clément <clab60917@eleve.isep.fr>
2023-10-16 18:07:14 +0200

    pb argon2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+//import { PassThrough } from 'stream';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'I have signed up' };
+
+  async signup(dto: AuthDto) {
+    //ça va generate le password
+    const hash = await argon.hash(dto.password);
+    //save user (new) in db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    //return saved user
+    return user;
   }
 
   signin() {
