ae6045e99eb550e69dd3c2cac252aa25ea56aa1d
Clément <clab60917@eleve.isep.fr>
2023-10-18 17:10:54 +0200

    signin token with JWC is working

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-//import { PassThrough } from 'stream';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
-    //ça va generate le password
+    // generate the password hash
     const hash = await argon.hash(dto.password);
-    //save user (new) in db
+    // save the new user in the db
     try {
       const user = await this.prisma.user.create({
         data: {
@@ -19,13 +25,12 @@ export class AuthService {
           hash,
         },
       });
-      delete user.hash;
-      //return saved user
-      return user;
+
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
-          throw new ForbiddenException('Creds already taken !');
+          throw new ForbiddenException('Credentials taken');
         }
       }
       throw error;
@@ -33,23 +38,39 @@ export class AuthService {
   }
 
   async signin(dto: AuthDto) {
-    // Find the user with the email
+    // find the user by email
     const user = await this.prisma.user.findUnique({
       where: {
         email: dto.email,
       },
     });
-    // if user =/ not exist --> throw exeption
-    if (!user) throw new ForbiddenException('Wrong creds !');
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
 
-    // if user exist --> compare the password
+    // compare password
     const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+    return this.signToken(user.id, user.email);
+  }
 
-    // if password =/ not match --> throw exeption
-    if (!pwMatches) throw new ForbiddenException('Wrong creds !');
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
 
-    // if password match --> return user
-    delete user.hash;
-    return user;
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
